02853c98e63afd270032699a759420b4e1343719
TheChain01 <hlach070@uottawa.ca>
2020-07-23 15:11:20 -0400

    Initial Files

---
+++ MapSearch/Global.swift
@@ -0,0 +1,20 @@
+//
+//  Global.swift
+//  MapSearch
+//
+//  Created by Hubert Lachaîne on 2020-07-23.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+struct Global {
+    
+    struct MapTempInfo {
+        static var mapItems = [""]
+        static var coordinates = [""]
+        static var timeStamps = [""]
+        
+    }
+}
---
+++ MapSearch/timeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  timeViewController.swift
+//  MapSearch
+//
+//  Created by Hubert Lachaîne on 2020-07-23.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class timeViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.title = "WHEN?"
+        navigationController?.navigationBar.backgroundColor = UIColor.systemOrange
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    @IBAction func submitPressed(_ sender: Any) {
+        var dateFormatter = DateFormatter()
+        
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .short
+        
+        var inputDate = dateFormatter.string(from: datePicker.date)
+        print(inputDate)
+        
+        Global.MapTempInfo.timeStamps.append(inputDate)
+        print(Global.MapTempInfo.timeStamps)
+        print(Global.MapTempInfo.mapItems)
+        print(Global.MapTempInfo.coordinates)
+        let x = Global.MapTempInfo.timeStamps.firstIndex(of: "")
+        if x == 0 {
+            Global.MapTempInfo.timeStamps.removeFirst()
+        }
+    }
+    
+}
